bce169fac113da4d7b0df6b14e19b8e87959860f
Irinapreo <irinapreobrazhenskaya100@gmail.com>
2023-09-23 15:37:17 +0200

    initial commit

---
+++ Klockan/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+int times = 0;
+for (int hour = 0; hour<24; hour++)
+{
+    for(int min= 0; min<60; min++)
+    {
+        for (int sec = 0; sec < 60; sec++)
+        {
+            Console.WriteLine(nolla(hour) + ":" + nolla(min) + ":" + nolla(sec));
+            times++;
+        }
+    }
+}
+string nolla(int number)
+{
+    if (number <= 9)
+    {
+        return "0" + number;
+    }
+    else
+    {
+        return number.ToString();
+    }
+}
